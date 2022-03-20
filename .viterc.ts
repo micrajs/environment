3e21605f35abaeeab8e69cd9038c99c5196cbe48
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-20 18:15:36 -0500

    fix: fixed build output for testing entry

--- .viterc.ts
+++ .viterc.ts
@@ -7,7 +7,7 @@ export default defineConfig({
       external: ['@micra/error', '@micra/event-emitter', 'vitest'],
       input: {
         index: cwd('index.ts'),
-        testing: cwd('testing/index.ts'),
+        'testing': cwd('testing.ts'),
       },
     },
   },
