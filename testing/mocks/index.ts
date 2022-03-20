b79bff501f20e780baf6febc9bca0d2580ad32e8
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-20 18:08:33 -0500

    feat: added testing layer

--- testing/mocks/index.ts
+++ testing/mocks/index.ts
@@ -1,14 +1,15 @@
+import {vi} from 'vitest';
+
 export const MockEnvironment = class MockEnvironment
   implements Micra.Environment
 {
-  all = jest.fn();
-  get = jest.fn();
-  has = jest.fn();
-  init = jest.fn();
-  initAsync = jest.fn();
-  setValidator = jest.fn();
-  validate = jest.fn();
-  on = jest.fn();
-  emit = jest.fn();
-  emitSync = jest.fn();
+  all = vi.fn();
+  get = vi.fn();
+  has = vi.fn();
+  init = vi.fn();
+  initSync = vi.fn();
+  validate = vi.fn();
+  on = vi.fn();
+  emit = vi.fn();
+  emitSync = vi.fn();
 };
